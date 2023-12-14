9e9050b8e9504b3f7a4d0d02c2a825408bf24d75
Andronikos <andro.kontos@gmail.com>
2023-12-14 14:42:50 +0200

    Add another lambda function and a DynamoDB table

--- hitcounter_api/hitcounter_api_stack.py
+++ hitcounter_api/hitcounter_api_stack.py
@@ -9,18 +9,27 @@ from aws_cdk import (
 # The API Gateway we are deploying proxies all requests to an AWS Lambda function
 # To test after deployment use 'curl https://xxxxxxxxxx.execute-api.us-east-1.amazonaws.com/prod/'
 
+from .hitcounter import HitCounter
+
 class CdkWorkshopStack(Stack):
 
     def __init__(self, scope: Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        # Defines an AWS Lambda resource with the handler from lambda/hello.py (path relative to root directory)
+        # Defines an AWS Lambda function with the handler from lambda/hello.py (path relative to root directory)
         my_lambda = _lambda.Function(
             self, 'HelloHandler',
             runtime=_lambda.Runtime.PYTHON_3_7,
             code=_lambda.Code.from_asset('lambda'),
             handler='hello.handler',
         )
+
+        # Defines an AWS Lambda function which updates the table and invokes the 'my_lambda' function
+        hello_with_counter = HitCounter(
+            self, 'HelloHitCounter',
+            downstream=my_lambda,
+        )
+
 
         # Defines an API endpoint and associates it with our Lambda function
         apigw.LambdaRestApi(
